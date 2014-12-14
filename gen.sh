31db2c0733bffa2f6afc441df10d80ba8b37a91f
Rafael Chaves <rafael@abstratt.com>
2014-12-14 21:51:36 -0200

    generating from develop.cloudfier.com

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 rm -Rf $1/$2/gen
 mkdir -p $1/$2/gen
-wget -v -d  http://localhost/services/generator/demo-cloudfier-examples-$2/platform/$1  -O generated.zip
+wget -v -d  http://develop.cloudfier.com/services/generator/test-cloudfier-examples-$2/platform/$1  -O generated.zip
 unzip -d $1/$2/gen generated.zip
 rm -Rf generated.zip
 ln -s ../../../node_modules $1/$2/gen/	
