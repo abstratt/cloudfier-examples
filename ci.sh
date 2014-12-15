8189f2203c08de46040f47a24a3b400ef5dee808
Rafael Chaves <rafael@abstratt.com>
2014-12-15 12:53:10 -0200

    CI

--- ci.sh
+++ ci.sh
@@ -18,5 +18,5 @@ rm -Rf node_modules
 npm install -p
 cd -
 echo Testing $APPLICATION on $PLATFORM 
-mkdir -p test-results
-./test.sh $PLATFORM $APPLICATION --reporter xunit 1> test-results/$PLATFORM-$APPLICATION-test-report.xml
+mkdir -p test-reports
+./test.sh $PLATFORM $APPLICATION --reporter xunit 1> test-reports/$PLATFORM-$APPLICATION-test-report.xml
