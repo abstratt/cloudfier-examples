c5f2fd135fb76a9640ee5f4ca56b25503b956f09
Rafael Chaves <rafael@abstratt.com>
2015-02-19 17:06:10 -0200

    creating link to node_modules

--- mean-test.sh
+++ mean-test.sh
@@ -4,4 +4,5 @@ if [ "$#" -lt 1 ] ; then
     exit 1
 fi
 APPLICATION=$1
+ln -s $PWD/node_modules/ $PLATFORM/$APPLICATION/gen/
 mean/$APPLICATION/gen/node_modules/mocha/bin/mocha -u tdd mean/$APPLICATION/gen/test $2 $3 $4 $5 $6 $7 $8 $9
