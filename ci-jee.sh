780d416fd8f605ea190db901c7d726a4df88bec3
Rafael Chaves <rafael@abstratt.com>
2015-02-20 00:51:21 -0200

    removed comments

--- ci-jee.sh
+++ ci-jee.sh
@@ -1,7 +1,7 @@
 for app_path in jee/* ; do
     app=${app_path##*/}
 	echo "Running tests for - $app"
-        echo ./gen.sh jee $app
-        echo ./test.sh jee $app -DreportsDirectory=$PWD/test-reports
+    ./gen.sh jee $app
+    ./test.sh jee $app -DreportsDirectory=$PWD/test-reports
 done
 cd -
