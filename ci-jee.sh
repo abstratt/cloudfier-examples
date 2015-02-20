3d9b2ef2015605922a227fe131dc2ec728fdcf34
Rafael Chaves <rafael@abstratt.com>
2015-02-20 13:30:51 -0200

    being flexible w.r.t. PWD var

--- ci-jee.sh
+++ ci-jee.sh
@@ -1,7 +1,10 @@
+CWD=$WORKSPACE
+: ${CWD:=$PWD}
+
 for app_path in jee/* ; do
 	app=${app_path##*/}
 	echo "Running tests for - $app"
 	./gen.sh jee $app
-	./test.sh jee $app -DreportsDirectory=$PWD/test-reports
+	./test.sh jee $app -DreportsDirectory=$CWD/test-reports
 done
 cd -
--- mean-test.sh
+++ mean-test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 if [ "$#" -lt 1 ] ; then
     echo 'Parameters APPLICATION is required'
     exit 1
 fi
+
+CWD=$WORKSPACE
+: ${CWD:=$PWD}
+
 APPLICATION=$1
-unlink $PWD/mean/$APPLICATION/gen/node_modules
-ln -s $PWD/node_modules $PWD/mean/$APPLICATION/gen/
+unlink $CWD/mean/$APPLICATION/gen/node_modules
+ln -s $CWD/node_modules $CWD/mean/$APPLICATION/gen/
 mean/$APPLICATION/gen/node_modules/mocha/bin/mocha -u tdd mean/$APPLICATION/gen/test $2 $3 $4 $5 $6 $7 $8 $9
