0fcbf02105b1206f4dda1730139bb17e864fc24f
Rafael Chaves <rafael@abstratt.com>
2015-02-20 13:38:11 -0200

    desperation

--- jee-test.sh
+++ jee-test.sh
@@ -4,4 +4,5 @@ if [ "$#" -lt 1 ] ; then
     exit 1
 fi
 APPLICATION=$1
+echo mvn clean install -f jee/$APPLICATION/gen/pom.xml $2 $3 $4 $5 $6 $7 $8 $9
 mvn clean install -f jee/$APPLICATION/gen/pom.xml $2 $3 $4 $5 $6 $7 $8 $9
