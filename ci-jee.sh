63ca53aaa51e1398554b870db7f5902fbae2765c
Rafael Chaves <rafael@abstratt.com>
2015-02-20 15:00:55 -0200

    handling paths with spaces

--- ci-jee.sh
+++ ci-jee.sh
@@ -5,6 +5,6 @@ for app_path in jee/* ; do
 	app=${app_path##*/}
 	echo "Running tests for - $app"
 	./gen.sh jee $app
-	./test.sh jee $app -DreportsDirectory=$CWD/test-reports
+	./test.sh jee $app "-DreportsDirectory=$CWD/test-reports"
 done
 cd -
