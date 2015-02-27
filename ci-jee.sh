c50556b502e394fc6612d930d9007d56bc54da00
Rafael Chaves <rafael@abstratt.com>
2015-02-27 13:55:08 -0300

    allow choosing a user to run under

--- ci-jee.sh
+++ ci-jee.sh
@@ -3,10 +3,12 @@ set -o pipefail
 CWD=$WORKSPACE
 : ${CWD:=$PWD}
 
+CLOUDFIER_USER=$1
+
 for app_path in jee/* ; do
 	app=${app_path##*/}
 	echo "Running tests for - $app"
-	./gen.sh jee $app
+	./gen.sh jee $app $CLOUDFIER_USER
 	./test.sh jee $app "-DreportsDirectory=$CWD/test-reports"
 done
 
